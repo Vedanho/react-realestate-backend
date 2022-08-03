b81616cc80faf9eced04d94ad6807855595a5aeb
khald <abv.khallid@mail.ru>
2022-08-03 17:33:05 +0300

    authorization

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const cors = require("cors")
 const express = require("express")
 const mongoose = require("mongoose")
-const app = express()
+const cookieParser = require("cookie-parser")
 require("dotenv").config()
 
+const app = express()
+
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors())
 
-// app.use(require("./routes/User.route"))
+app.use(require("./routes/user.route"))
 
 mongoose
   .connect(process.env.MONGO_SERVER)
