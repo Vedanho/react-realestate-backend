e6a003c689fb655efdc69e21040f63d2f4566b53
Inasalamov Amirkhan <amirhan.inasalamov@mail.ru>
2022-11-24 15:44:37 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.use(require("./Apartment.route"));
+router.use(require("./apartment.route"));
 
 router.use(require("./realtors.route"));
 
